5069e8bae5a035222150ca6897f5a451e9c4b679
NelonArrive <nelon.arrive@gmail.com>
2026-02-06 01:04:29 +0500

    two pointers - day 2

---
+++ two-pointers/src/main/java/dev/nelon/algorithms/twopointers/TwoSum_2.java
@@ -0,0 +1,29 @@
+package dev.nelon.algorithms.twopointers;
+
+import java.util.Arrays;
+
+public class TwoSum_2 {
+	public static void main(String[] args) {
+		int[] nums = {2, 7, 11, 15};
+		System.out.println(Arrays.toString(twoSum(nums, 9)));
+	}
+	
+	public static int[] twoSum(int[] numbers, int target) {
+		int left = 0;
+		int right = numbers.length - 1;
+		
+		while (left < right) {
+			int sum = numbers[left] + numbers[right];
+			
+			if (sum == target) {
+				return new int[]{left + 1, right + 1};
+			} else if (sum < target) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+		
+		return new int[]{-1, -1};
+	}
+}
---
+++ two-pointers/src/main/java/dev/nelon/algorithms/twopointers/ValidPalindrome.java
@@ -0,0 +1,33 @@
+package dev.nelon.algorithms.twopointers;
+
+public class ValidPalindrome {
+	public static void main(String[] args) {
+		String s = "A man, a plan, a canal: Panama";
+		
+		System.out.println(isPalindrome(s));
+	}
+	
+	private static boolean isPalindrome(String s) {
+		int left = 0;
+		int right = s.length() - 1;
+		
+		while (left < right) {
+			char l = s.charAt(left);
+			char r = s.charAt(right);
+			
+			if (!Character.isLetterOrDigit(l)) {
+				left++;
+			} else if (!Character.isLetterOrDigit(r)) {
+				right--;
+			} else {
+				if (Character.toLowerCase(l) != Character.toLowerCase(r)) {
+					return false;
+				}
+				left++;
+				right--;
+			}
+		}
+		
+		return true;
+	}
+}
