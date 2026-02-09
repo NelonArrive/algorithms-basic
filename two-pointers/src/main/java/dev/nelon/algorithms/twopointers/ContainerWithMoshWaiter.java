57c4c42777182c21bf6f4c2deb0f24f479a57d05
NelonArrive <nelon.arrive@gmail.com>
2026-02-09 22:42:46 +0500

    day 3 - Two Pointers

---
+++ two-pointers/src/main/java/dev/nelon/algorithms/twopointers/ContainerWithMoshWaiter.java
@@ -0,0 +1,15 @@
+package dev.nelon.algorithms.twopointers;
+
+import java.util.Arrays;
+
+public class ContainerWithMoshWaiter {
+  public static void main(String[] args) {
+    int[] height = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+    System.out.println(Arrays.toString(maxArea(height)));
+  }
+
+  public static int[] maxArea(int[] height) {
+
+    return null;
+  }
+}
---
+++ two-pointers/src/main/java/dev/nelon/algorithms/twopointers/MoveZeroes.java
@@ -0,0 +1,25 @@
+package dev.nelon.algorithms.twopointers;
+
+import java.util.Arrays;
+
+public class MoveZeroes {
+  public static void main(String[] args) {
+    int[] nums = {0, 1, 0, 3, 12};
+    System.out.println(Arrays.toString(moveZeroes(nums)));
+  }
+
+  public static int[] moveZeroes(int[] nums) {
+    int left = 0;
+
+    for (int rigth = 0; rigth < nums.length; rigth++) {
+      if (nums[rigth] != 0) {
+        int temp = nums[left];
+        nums[left] = nums[rigth];
+        nums[rigth] = temp;
+        left++;
+      }
+    }
+
+    return nums;
+  }
+}
